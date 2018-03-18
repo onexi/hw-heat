28247d0a3db9aa5ca129213f4cd63d75c56fcc4e
jleapeMIT <jleape@mit.edu>
2018-03-18 17:08:23 -0400

    Updated exercise file(s)

--- jleapeMIT/exercise.js
+++ jleapeMIT/exercise.js
@@ -12,8 +12,7 @@ exercise.countDistrictCrimes = function(data,district){
     // Return the number of crimes 
     // for the given district
     //-------------------------------------------
-    const result = data.filter(crime => crime[12] === district);
-    console.log(result.length);
+    const result = data.filter(crime => crime[19] == district);
     return result.length;
 };
 
@@ -22,7 +21,7 @@ exercise.countPrimaryType = function(data,primaryType){
     // Return the number of crimes 
     // for the giving primary type
     //-------------------------------------------
-    const result = data.filter(crime => crime[6] === primaryType);
+    const result = data.filter(crime => crime[13] === primaryType);
     console.log(result.length);
     return result.length;
 };
@@ -32,7 +31,7 @@ exercise.countLocation = function(data,location){
     // Return the number of crimes 
     // for the given location
     //-------------------------------------------
-    const result = data.filter(crime => crime[22] === location);
+    const result = data.filter(crime => crime[29] === location);
     console.log(result.length);
     return result.length;
 };
@@ -43,8 +42,8 @@ exercise.buildLatLngPoint = function(crime){
     // build the point for the given crime
     //-------------------------------------------
     var point = {};    
-    point.latitude = crime[19];
-    point.longitude = crime[21];
+    point.latitude = crime[26];
+    point.longitude = crime[28];
     return point; 
 };
 
