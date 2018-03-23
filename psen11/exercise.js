69f9d0d93144fcbc8fe6a0a613ca5befe7a80383
psen11 <senp@mit.edu>
2018-03-22 23:11:13 -0400

    Updated exercise file(s)

--- psen11/exercise.js
+++ psen11/exercise.js
@@ -1,11 +1,13 @@
 var exercise = {};
 
 exercise.countRecords = function(data){
+   
     //-------------------------------------------
     // YOUR CODE
     // Return the total number of records
     //-------------------------------------------
-    return 'Error: countRecords not implemented'; 
+    
+    return data.length; 
 };
 
 exercise.countDistrictCrimes = function(data,district){
@@ -14,41 +16,58 @@ exercise.countDistrictCrimes = function(data,district){
     // Return the number of crimes 
     // for the given district
     //-------------------------------------------
-    return 'Error: countDistrictCrimes not implemented'; 
+    var count = 0;
+    data.forEach(function(item,index){
+        dist = item[19];
+        //console.log(dist);
+        //console.log(district);
+        if (dist==district){
+            count = count+1;
+        };
+     });
+     return count; 
 };
 
 exercise.countPrimaryType = function(data,primaryType){
+    
+    var number = 0;
+    data.forEach(function(item,index){
+        if(item[13]==primaryType){
+            number = number+1;
+        };
+    });
     //-------------------------------------------
     // YOUR CODE
     // Return the number of crimes 
     // for the giving primary type
     //-------------------------------------------
-    return 'Error: countPrimaryType not implemented';
+    return number;
 };
 
 exercise.countLocation = function(data,location){
+    var count = 0;
+    data.forEach(function(item,index){
+        //console.log(item[15]);  
+        if (item[15]==location){
+            count = count+1;
+        };
+    });
+    
     //-------------------------------------------
     // YOUR CODE
     // Return the number of crimes 
     // for the given location
     //-------------------------------------------
-    return 'Error: countLocation not implemented';
+    return count;
 };
 
 
 exercise.buildLatLngPoint = function(crime){
-    //-------------------------------------------
-    // YOUR CODE
-    // build the point for the given crime
-    // 
-    // For example:
-    //
-    // var point = {};    
-    // point.latitude = crime[latitudeColumnNumber];
-    // point.longitude = crime[longitudeColumnNumber];
-    // return point;    
-    //-------------------------------------------    
-    return 'Error: buildLatLngPoint not implemented';
+   
+    var point = {};
+    point.latitude = crime[29][1];
+    point.longitude = crime[29][2];
+    return point;
 };
 
 
