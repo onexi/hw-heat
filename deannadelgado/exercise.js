cea2e7161f04ade238dceb585ffcf2cb144ff02a
deannadelgado <deannad@mit.edu>
2018-04-06 09:28:22 -0400

    Updated exercise file(s)

--- deannadelgado/exercise.js
+++ deannadelgado/exercise.js
@@ -2,37 +2,65 @@ var exercise = {};
 
 exercise.countRecords = function(data){
     //-------------------------------------------
-    // YOUR CODE
+    
+    return Object.keys(chicago.data).length;
+
     // Return the total number of records
     //-------------------------------------------
-    return 'Error: countRecords not implemented'; 
+   // return 'Error: countRecords not implemented'; 
 };
 
 exercise.countDistrictCrimes = function(data,district){
     //-------------------------------------------
-    // YOUR CODE
+    var countingDistrict = data.filter(function(report){
+        return report[18] == district;
+    });
+
+    var crimes = {};
+    crimes.district = district;
+    crimes.reports = counting.length;
+
+    return crimes;
+
     // Return the number of crimes 
     // for the given district
     //-------------------------------------------
-    return 'Error: countDistrictCrimes not implemented'; 
+    //return 'Error: countDistrictCrimes not implemented'; 
 };
 
 exercise.countPrimaryType = function(data,primaryType){
     //-------------------------------------------
-    // YOUR CODE
+    var countingPrimary = data.filter(function(report2){
+        return report2[18] == primary;
+    });
+
+    var crimes = {};
+    crimes.primary = primary;
+    crimes.reports = countingPrimary.length;
+
+    return crimes;
+
     // Return the number of crimes 
     // for the giving primary type
     //-------------------------------------------
-    return 'Error: countPrimaryType not implemented';
+    //return 'Error: countPrimaryType not implemented';
 };
 
 exercise.countLocation = function(data,location){
     //-------------------------------------------
-    // YOUR CODE
+    var countingLocation = data.filter(function(report3){
+        return report3[18] == location;
+    });
+
+    var crimes = {};
+    crimes.location = location;
+    crimes.reports = countingLocation.length;
+
+    return crimes;
     // Return the number of crimes 
     // for the given location
     //-------------------------------------------
-    return 'Error: countLocation not implemented';
+    //return 'Error: countLocation not implemented';
 };
 
 
@@ -41,14 +69,13 @@ exercise.buildLatLngPoint = function(crime){
     // YOUR CODE
     // build the point for the given crime
     // 
-    // For example:
     //
-    // var point = {};    
-    // point.latitude = crime[latitudeColumnNumber];
-    // point.longitude = crime[longitudeColumnNumber];
-    // return point;    
+    var point = {};    
+    point.latitude = crime[latitudeColumnNumber];
+    point.longitude = crime[longitudeColumnNumber];
+    return point;    
     //-------------------------------------------    
-    return 'Error: buildLatLngPoint not implemented';
+    //return 'Error: buildLatLngPoint not implemented';
 };
 
 
